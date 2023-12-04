1d61842164e4a4ca8affd1ce51892e23d3a322bc
eojin0814 <leeeojin6@gmail.com>
2023-12-05 00:04:30 +0900

    feat: product 전체 조회 api

--- src/main/java/com/samsung/samsung/domain/product/controller/ProductController.java
+++ src/main/java/com/samsung/samsung/domain/product/controller/ProductController.java
@@ -1,14 +1,23 @@
 package com.samsung.samsung.domain.product.controller;
 
+import com.samsung.samsung.domain.product.dto.ProductDto;
 import com.samsung.samsung.domain.product.service.ProductService;
+import com.samsung.samsung.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
-    
+    @GetMapping("/list")
+    public BaseResponse<List<ProductDto.Response>> getProductList(){
+        List<ProductDto.Response> productList = productService.findByAll();
+        return BaseResponse.onSuccess(productList);
+    }
 }
